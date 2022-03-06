71acca8def8d0eca3c9204937ddac6bdfa0c71f8
Michael Tao <mtao@dgp.toronto.edu>
2022-03-05 16:16:17 -0800

    slight update to node

--- include/balsa/visualization/vulkan/objects/node.hpp
+++ include/balsa/visualization/vulkan/objects/node.hpp
@@ -1,6 +1,7 @@
 #if !defined(BALSA_VISUALIZATION_VULKAN_OBJECTS_NODE_HPP)
 #define BALSA_VISUALIZATION_VULKAN_OBJECTS_NODE_HPP
 #include <memory>
+#include <vector>
 
 namespace balsa::visualization::vulkan {
 class Camera;
@@ -8,9 +9,14 @@ class Film;
 }// namespace balsa::visualization::vulkan
 namespace balsa::visualization::vulkan::objects {
 struct Node : public std::enable_shared_from_this<Node> {
+    using Ptr = std::shared_ptr<Node>;
     virtual ~Node();
 
-    virtual void draw(const Camera &cam, Film &film) = 0;
+    virtual void draw(const Camera &cam, Film &film) const;
+    void draw_children(const Camera &cam, Film &film) const;
+
+    private:
+    std::vector<Node> _children;
 };
 }// namespace balsa::visualization::vulkan::objects
 
