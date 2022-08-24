76b49961ad6e17f90c924bfb43c79c498d2113ce
Michael Tao <mtao@dgp.toronto.edu>
2022-08-24 11:58:16 -0700

    minor progress to block matrix impl

--- include/balsa/data_structures/stacked_contiguous_buffer.hpp
+++ include/balsa/data_structures/stacked_contiguous_buffer.hpp
@@ -6,35 +6,36 @@
 
 
 namespace balsa::data_structures {
-template<typename IndexType = int>
+template<typename ValueType >
 struct StackedContiguousBuffer {
 
 
+    // return a Block holding the data
     auto get_span(int index) const;
     auto get_span_offsets(int index) const;
 
     size_t span_count() const {
         return _offsets.size() - 1;
     }
-    balsa::eigen::VectorX<IndexType> _buffer;
+    balsa::eigen::VectorX<ValueType> _buffer;
     balsa::eigen::VectorX<int> _offsets;
 
     bool operator==(const StackedContiguousBuffer &o) const;
 };
 
-template<typename IndexType>
-auto StackedContiguousBuffer<IndexType>::get_span_offsets(int index) const {
+template<typename ValueType>
+auto StackedContiguousBuffer<ValueType>::get_span_offsets(int index) const {
     return _offsets.segment<2>(index);
 }
 
-template<typename IndexType>
-auto StackedContiguousBuffer<IndexType>::get_span(int index) const {
+template<typename ValueType>
+auto StackedContiguousBuffer<ValueType>::get_span(int index) const {
     auto offsets = get_span_offsets(index);
-    return _buffer.segment(offsets(0), offsets(1) - offsets(0));
+    return _buffer(Eigen::seq(offsets(0), offsets(1)));
 }
 
-template<typename IndexType>
-bool StackedContiguousBuffer<IndexType>::operator==(const StackedContiguousBuffer &o) const {
+template<typename ValueType>
+bool StackedContiguousBuffer<ValueType>::operator==(const StackedContiguousBuffer &o) const {
     return _offsets == o._offsets && o._buffer == _buffer;
 }
 }// namespace balsa::data_structures
--- include/balsa/eigen/element_block_matrix.hpp
+++ include/balsa/eigen/element_block_matrix.hpp
@@ -1,4 +1,3 @@
-
 #if !defined(BALSA_EIGEN_SHAPE_CHECKS_HPP)
 #define BALSA_EIGEN_SHAPE_CHECKS_HPP
 #include <Eigen/Core>
@@ -8,26 +7,25 @@
 
 namespace balsa::eigen {
 
-    template <concepts::EigenBaseDerived BlockMatrixType>
+template<concepts::EigenBaseDerived BlockMatrixType>
 class ElementBlockMatrix;
 }
- 
+
 namespace Eigen {
 namespace internal {
-  // ElementBlockMatrix looks-like a SparseMatrix, so let's inherits its traits:
-    template <balsa::eigen::concepts::EigenBaseDerived BlockMatrixType>
-  struct traits<balsa::eigen::ElementBlockMatrix<BlockMatrixType>> :  public Eigen::internal::traits<Eigen::SparseMatrix<double> >
-  {};
+    // ElementBlockMatrix looks-like a SparseMatrix, so let's inherits its traits:
+    template<balsa::eigen::concepts::EigenBaseDerived BlockMatrixType>
+    struct traits<balsa::eigen::ElementBlockMatrix<BlockMatrixType>> : public Eigen::internal::traits<Eigen::SparseMatrix<double>> {};
 }
 }
- 
+
 namespace balsa::eigen {
 
 
 //    namespace detail {
 //    template <concepts::EigenBaseDerived MatrixType_>
-//class MatrixBlock : public Eigen::EigenBase<MatrixBlock<MatrixType_> > {
-//public:
+// class MatrixBlock : public Eigen::EigenBase<MatrixBlock<MatrixType_> > {
+// public:
 //    using MatrixType = MatrixType_;
 //  // Required typedefs, constants, and method:
 //    using Scalar = typename MatrixType::Scalar;
@@ -39,17 +37,17 @@ namespace balsa::eigen {
 //    MaxColsAtCompileTime = Eigen::Dynamic,
 //    IsRowMajor = Eigen::internal::template traits<MatrixType>::IsRowMajor
 //  };
-// 
-// 
+//
+//
 //  template<typename Rhs>
 //  Eigen::Product<MatrixBlock,Rhs,Eigen::AliasFreeProduct> operator*(const Eigen::MatrixBase<Rhs>& x) const {
 //    return Eigen::Product<MatrixBlock,Rhs,Eigen::AliasFreeProduct>(*this, x.derived());
 //  }
-// 
-// 
-//private:
 //
-//  MatrixType 
+//
+// private:
+//
+//  MatrixType
 //  std::vector<Index> _rows;
 //  std::vector<Index> _cols;
 //
@@ -59,69 +57,72 @@ namespace balsa::eigen {
 
 // Example of a matrix-free wrapper from a user type to Eigen's compatible type
 // For the sake of simplicity, this example simply wrap a Eigen::SparseMatrix.
-    template <concepts::EigenBaseDerived BlockMatrixType_>
-class ElementBlockMatrix : public Eigen::EigenBase<ElementBlockMatrix<BlockMatrixType_> > {
-public:
+template<concepts::EigenBaseDerived BlockMatrixType_>
+class ElementBlockMatrix : public Eigen::EigenBase<ElementBlockMatrix<BlockMatrixType_>> {
+  public:
     using BlockMatrixType = BlockMatrixType_;
-  // Required typedefs, constants, and method:
+    // Required typedefs, constants, and method:
     using Scalar = typename BlockMatrixType::Scalar;
     using RealScalar = Scalar;
     using StorageIndex = typename BlockMatrixType::StorageIndex;
     using Index = Eigen::Index;
-  enum {
-    ColsAtCompileTime = Eigen::Dynamic,
-    MaxColsAtCompileTime = Eigen::Dynamic,
-    IsRowMajor = Eigen::internal::template traits<BlockMatrixType>::IsRowMajor
-  };
- 
-  Index rows() const { return _num_rows; }
-  Index cols() const { return _num_cols; }
- 
-  template<typename Rhs>
-  Eigen::Product<ElementBlockMatrix,Rhs,Eigen::AliasFreeProduct> operator*(const Eigen::MatrixBase<Rhs>& x) const {
-    return Eigen::Product<ElementBlockMatrix,Rhs,Eigen::AliasFreeProduct>(*this, x.derived());
-  }
+    enum {
+        ColsAtCompileTime = Eigen::Dynamic,
+        MaxColsAtCompileTime = Eigen::Dynamic,
+        IsRowMajor = Eigen::internal::template traits<BlockMatrixType>::IsRowMajor
+    };
 
-  const BlockMatrixType& get_block(size_t index) const { return h:
- 
-  // Custom API:
-private:
+    Index rows() const { return _num_rows; }
+    Index cols() const { return _num_cols; }
 
+    template<typename Rhs>
+    Eigen::Product<ElementBlockMatrix, Rhs, Eigen::AliasFreeProduct> operator*(const Eigen::MatrixBase<Rhs> &x) const {
+        return Eigen::Product<ElementBlockMatrix, Rhs, Eigen::AliasFreeProduct>(*this, x.derived());
+    }
 
-  data_structures::StackedContiguousBuffer<Index> _row_blocks;
-  data_structures::StackedContiguousBuffer<Index> _col_blocks;
-  std::vector<BlockMatrixType> _blocks;
+    const BlockMatrixType &get_block(size_t index) const {
+        return _blocks.at(index);
+    }
+    const 
+        const auto &row_block = _row_blocks.get_span(index);
+        const auto &col_block = _col_blocks.get_span(index);
+    }
 
-  Index _num_rows;
-  Index _num_cols;
+    // Custom API:
+  private:
+    data_structures::StackedContiguousBuffer<Index>
+      _row_blocks;
+    data_structures::StackedContiguousBuffer<Index> _col_blocks;
+    std::vector<BlockMatrixType> _blocks;
+
+    Index _num_rows;
+    Index _num_cols;
 };
-    
+
 
 }// namespace balsa::eigen
 // Implementation of ElementBlockMatrix * Eigen::DenseVector though a specialization of internal::generic_product_impl:
 namespace Eigen {
 namespace internal {
- 
-  template<typename Rhs>
-  struct generic_product_impl<vem::ElementBlockMatrix, Rhs, SparseShape, DenseShape, GemvProduct> // GEMV stands for matrix-vector
-  : generic_product_impl_base<vem::ElementBlockMatrix,Rhs,generic_product_impl<balsa::eigen::ElementBlockMatrix,Rhs> >
-  {
-    typedef typename Product<vem::ElementBlockMatrix,Rhs>::Scalar Scalar;
- 
-    template<typename Dest>
-    static void scaleAndAddTo(Dest& dst, const ElementBlockMatrix& lhs, const Rhs& rhs, const Scalar& alpha)
-    {
-      // This method should implement "dst += alpha * lhs * rhs" inplace,
-      // however, for iterative solvers, alpha is always equal to 1, so let's not bother about it.
-      assert(alpha==Scalar(1) && "scaling is not implemented");
-      EIGEN_ONLY_USED_FOR_DEBUG(alpha);
- 
-      // Here we could simply call dst.noalias() += lhs.my_matrix() * rhs,
-      // but let's do something fancier (and less efficient):
-      for(Index i=0; i<lhs.cols(); ++i)
-        dst += rhs(i) * lhs.my_matrix().col(i);
-    }
-  };
- 
+
+    template<typename Rhs>
+    struct generic_product_impl<vem::ElementBlockMatrix, Rhs, SparseShape, DenseShape, GemvProduct>// GEMV stands for matrix-vector
+      : generic_product_impl_base<vem::ElementBlockMatrix, Rhs, generic_product_impl<balsa::eigen::ElementBlockMatrix, Rhs>> {
+        typedef typename Product<vem::ElementBlockMatrix, Rhs>::Scalar Scalar;
+
+        template<typename Dest>
+        static void scaleAndAddTo(Dest &dst, const ElementBlockMatrix &lhs, const Rhs &rhs, const Scalar &alpha) {
+            // This method should implement "dst += alpha * lhs * rhs" inplace,
+            // however, for iterative solvers, alpha is always equal to 1, so let's not bother about it.
+            assert(alpha == Scalar(1) && "scaling is not implemented");
+            EIGEN_ONLY_USED_FOR_DEBUG(alpha);
+
+            // Here we could simply call dst.noalias() += lhs.my_matrix() * rhs,
+            // but let's do something fancier (and less efficient):
+            for (Index i = 0; i < lhs.cols(); ++i)
+                dst += rhs(i) * lhs.my_matrix().col(i);
+        }
+    };
+
 }
 #endif
