6d3c327d499cf839d383f61e5d878958b89de09e
Michael Tao <michael.tao@nyu.edu>
2025-04-03 10:24:29 -0400

    starting to add support for zipper

---
+++ include/balsa/eigen/zipper_compat.hpp
@@ -0,0 +1,10 @@
+#if !defined(BALSA_EIGEN_ZIPPER_COMPAT_HPP)
+#define  BALSA_EIGEN_ZIPPER_COMPAT_HPP
+
+#include <zipper/Matrix.hpp>
+#include <zipper/Vector.hpp>
+
+namespace balsa::eigen {
+
+}
+#endif
---
+++ include/balsa/geometry/point_cloud/uniformly_sample_simplex.hpp
@@ -0,0 +1,19 @@
+
+#if !defined(BALSA_GEOMETRY_POINT_CLOUD_UNIFORMLY_SAMPLE_SIMPLEX_HPP)
+#define BALSA_GEOMETRY_POINT_CLOUD_UNIFORMLY_SAMPLE_SIMPLEX_HPP
+
+#include "balsa/eigen/types.hpp"
+#include <array>
+
+namespace balsa::geometry::point_cloud {
+
+//Read an xyz file assuming that it returns points and maybe 3 more values,
+//which are returned in two arrays
+//If any vertices lack normals the second array is empty
+std::array<balsa::eigen::ColVectors<float, 3>, 2> read_xyzF(const std::string &filename);
+std::array<balsa::eigen::ColVectors<double, 3>, 2> read_xyzD(const std::string &filename);
+
+}// namespace balsa::geometry::point_cloud
+
+#endif
+
