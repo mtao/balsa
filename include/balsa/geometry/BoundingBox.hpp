fe611600c78678edfc138e4fb51ee7bc22092d38
Michael Tao <mtao@dgp.toronto.edu>
2025-06-16 21:17:51 -0400

    bbox seems to work again

--- include/balsa/geometry/BoundingBox.hpp
+++ include/balsa/geometry/BoundingBox.hpp
@@ -2,6 +2,7 @@
 #define BALSA_GEOMETRY_BOUNDINGBOX
 #include <zipper/types.hpp>
 #include <zipper/Vector.hpp>
+#include <zipper/as.hpp>
 #include <zipper/views/nullary/ConstantView.hpp>
 #include <zipper/concepts/VectorBaseDerived.hpp>
 #include <limits>
@@ -22,12 +23,17 @@ class BoundingBox {
     template<
       zipper::concepts::VectorBaseDerived Vec>
     BoundingBox(const Vec &m);
-    BoundingBox();
+    BoundingBox() = default;
+
+    BoundingBox(BoundingBox &&) = default;
+    BoundingBox(const BoundingBox &) = default;
+    BoundingBox &operator=(BoundingBox &&) = default;
+    BoundingBox &operator=(const BoundingBox &) = default;
 
     BoundingBox(zipper::index_type dim) :
 
                                           m_min(zipper::views::nullary::ConstantView<T>(zipper::create_dextents(dim), limits::max())),
-                                          m_max(zipper::views::nullary::ConstantView<T>(zipper::create_dextents(dim), limits::min())) {}
+                                          m_max(zipper::views::nullary::ConstantView<T>(zipper::create_dextents(dim), limits::lowest())) {}
 
     template<zipper::concepts::VectorBaseDerived Vec>
     void expand(const Vec &a);
@@ -37,55 +43,86 @@ class BoundingBox {
     bool contains(const Vec &a) const;
     bool contains(const BoundingBox &a) const;
 
-    auto min() const { return m_min; }
-    auto max() const { return m_max; }
+    auto range() const {
+        return m_max - m_min;
+    }
 
+    const auto &min() const { return m_min; }
+    const auto &max() const { return m_max; }
 
   private:
     zipper::Vector<T, Dim> m_min = zipper::views::nullary::ConstantView<T>(limits::max());
-    zipper::Vector<T, Dim> m_max = zipper::views::nullary::ConstantView<T>(limits::min());
+    zipper::Vector<T, Dim> m_max = zipper::views::nullary::ConstantView<T>(limits::lowest());
 };
+
+
+// make sure the types are teh same and the extents are valid
+template<
+  zipper::concepts::VectorBaseDerived MinVec,
+  zipper::concepts::VectorBaseDerived MaxVec>
+    requires(std::is_same_v<typename MinVec::value_type, typename MaxVec::value_type>
+             && (MinVec::extents_type::static_extent(0) == std::dynamic_extent
+                 || MinVec::extents_type::static_extent(0) == std::dynamic_extent || MaxVec::extents_type::static_extent(0) == MinVec::extents_type::static_extent(0)))
+BoundingBox(const MinVec &m, const MaxVec &M) -> BoundingBox<typename MinVec::value_type,
+                                                             MinVec::extents_type::static_extent(0) == std::dynamic_extent ? MaxVec::extents_type::static_extent(0) : MinVec::extents_type::static_extent(0)>;
+
+
+template<
+  zipper::concepts::VectorBaseDerived Vec>
+BoundingBox(const Vec &) -> BoundingBox<typename Vec::value_type,
+                                        Vec::extents_type::static_extent(0)>;
+
 
 template<typename T, zipper::rank_type Dim>
 template<
   zipper::concepts::VectorBaseDerived MinVec,
   zipper::concepts::VectorBaseDerived MaxVec>
-BoundingBox<T, Dim>::BoundingBox(const MinVec &m, const MaxVec &M) : m_min(m), m_max(M) {}
+BoundingBox<T, Dim>::BoundingBox(const MinVec &m, const MaxVec &M) : m_min(m), m_max(M) {
+
+    constexpr bool min_ext = MinVec::extents_type::static_extent(0) == std::dynamic_extent;
+    constexpr bool max_ext = MaxVec::extents_type::static_extent(0) == std::dynamic_extent;
+    if constexpr (min_ext && max_ext) {
+        assert(m.extent(0) == M.extent(0));
+    } else if constexpr (min_ext) {
+        assert(m.extent(0) == Dim);
+    } else if constexpr (max_ext) {
+        assert(M.extent(0) == Dim);
+    }
+}
 
 template<typename T, zipper::rank_type Dim>
 template<
   zipper::concepts::VectorBaseDerived Vec>
 BoundingBox<T, Dim>::BoundingBox(const Vec &m) : m_min(m), m_max(m) {
 }
-template<typename T, zipper::rank_type Dim>
-BoundingBox<T, Dim>::BoundingBox() {}
 
 
 template<typename T, zipper::rank_type Dim>
 template<zipper::concepts::VectorBaseDerived Vec>
 void BoundingBox<T, Dim>::expand(const Vec &a) {
 
-    m_min = zipper::min(a.as_array(), m_min.as_array());
-    m_max = zipper::max(a.as_array(), m_max.as_array());
+    m_min = zipper::as_vector(zipper::min(a.as_array(), m_min.as_array()));
+    m_max = zipper::as_vector(zipper::max(a.as_array(), m_max.as_array()));
 }
 
 template<typename T, zipper::rank_type Dim>
 void BoundingBox<T, Dim>::expand(const BoundingBox &a) {
 
-    m_min = zipper::min(a.m_min.as_array(), m_min.as_array());
-    m_max = zipper::max(a.m_max.as_array(), m_max.as_array());
+    m_min = zipper::as_vector(zipper::min(a.m_min.as_array(), m_min.as_array()));
+    m_max = zipper::as_vector(zipper::max(a.m_max.as_array(), m_max.as_array()));
 }
 template<typename T, zipper::rank_type Dim>
 template<zipper::concepts::VectorBaseDerived Vec>
-bool BoundingBox<T, Dim>::contains(const Vec &a) const {
+bool BoundingBox<T, Dim>::contains(const Vec &x) const {
 
-    (a.as_array() >= m_min.as_array()).all() && (a.as_array() <= m_max.as_array()).all();
+    auto xa = x.array();
+    return ((m_min.array() <= xa) && (m_max.array() >= xa)).all();
 }
 
 template<typename T, zipper::rank_type Dim>
-bool BoundingBox<T, Dim>::contains(const BoundingBox &a) const {
+bool BoundingBox<T, Dim>::contains(const BoundingBox &o) const {
 
-    (a.m_min.as_array() >= m_min.as_array()).all() && (a.m_max.as_array() <= m_max.as_array()).all();
+    ((m_min.array() <= o.m_min.array()) && (m_max.array() >= o.m_max.array())).all();
 }
 }// namespace balsa::geometry
 #endif
--- tests/test_bounding_box.cpp
+++ tests/test_bounding_box.cpp
@@ -5,23 +5,45 @@
 #include <balsa/zipper/types.hpp>
 #include <zipper/views/nullary/ConstantView.hpp>
 #include <zipper/views/nullary/RandomView.hpp>
+
+#include <bitset>
 using namespace Catch;
 
 TEST_CASE("bounding_box", "[bounding_box][geometry]") {
     {
-            balsa::zipper::ColVectors<float, 3> V(3, 5);
-            V.col(0) = {0,0,0};
-            V.col(1) = {0,0,0};
-            V.col(2) = {0,0,0};
-            V.col(3) = {0,0,0};
-            V.col(4) = {0,0,0};
+        balsa::zipper::ColVectors<float, 3> V(3, 5);
+        V.col(0) = { 0, 0, 0 };
+        V.col(1) = { 0, 0, 0 };
+        V.col(2) = { 0, 0, 0 };
+        V.col(3) = { 0, 0, 0 };
+        V.col(4) = { 0, 0, 0 };
 
-            auto bb = balsa::geometry::bounding_box(V);
-
-
-            CHECK(bb.min() == V.row(0));
-            CHECK(bb.max() == V.row(0));
+        auto bb = balsa::geometry::bounding_box(V);
 
 
+        spdlog::info("{} {}", fmt::join(bb.min(), ","), fmt::join(zipper::Vector(V.col(0)), ","));
+        spdlog::info("{} {}", fmt::join(bb.max(), ","), fmt::join(zipper::Vector(V.col(0)), ","));
+        CHECK(bb.min() == zipper::Vector<float, 3>({ 0, 0, 0 }));
+        CHECK(bb.min() == zipper::Vector<float, 3>({ 0, 0, 0 }));
+        CHECK(bb.max() == bb.min());
+
+
+        V.col(1) = { 1, 3, 4 };
+        V.col(2) = { -2, -5, 20 };
+        bb = balsa::geometry::bounding_box(V);
+
+        CHECK(bb.min() == zipper::Vector<float, 3>({ -2, -5, 0 }));
+        CHECK(bb.max() == zipper::Vector<float, 3>({ 1, 3, 20 }));
+    }
+    {
+
+        balsa::geometry::BoundingBox bb(zipper::Vector<float, 2>({ 0, 1 }));
+        balsa::geometry::BoundingBox bb2(zipper::Vector<float, 2>({ 4, 4 }));
+
+        auto bb3 = bb;
+        bb3.expand(bb2);
+
+        CHECK(bb3.min() == bb.min());
+        CHECK(bb3.max() == bb2.max());
     }
 }
