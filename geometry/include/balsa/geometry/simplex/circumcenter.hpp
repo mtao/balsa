8d87671179c5c22c183559fcb73d0222545fb8a7
Michael Tao <michael.w.tao@gmail.com>
2026-03-15 11:21:43 -0400

    migrate circumcenter from Eigen QR/LLT to zipper native QR
    Replace Eigen's colPivHouseholderQr and LLT solvers with zipper's native
    Householder QR decomposition for the circumcenter computation:
    
    - Add detail::upper_triangular_solve() for back-substitution on R*x = b
    - Add detail::qr_solve() convenience wrapper (QR + Q^T*b + back-sub)
    - Add detail::qr_is_degenerate() to check R diagonal for rank deficiency
    - Rewrite circumcenter_spsd() to use qr_solve() instead of Eigen
    - Rewrite circumcenter_spd() to use zipper QR with degeneracy check
    - Remove Eigen/Dense and eigen/zipper_compat.hpp includes

--- geometry/include/balsa/geometry/simplex/circumcenter.hpp
+++ geometry/include/balsa/geometry/simplex/circumcenter.hpp
@@ -4,39 +4,76 @@
 #include <spdlog/spdlog.h>
 #include <stdexcept>
 #include <zipper/utils/extents/extent_arithmetic.hpp>
-#include <zipper/views/nullary/ConstantView.hpp>
+#include <zipper/expression/nullary/Constant.hpp>
+#include <zipper/utils/decomposition/qr.hpp>
 #include <tuple>
-#include "balsa/eigen/zipper_compat.hpp"
 #include "balsa/zipper/types.hpp"
-#include "zipper/concepts/MatrixBaseDerived.hpp"
-
-
-#include <Eigen/Dense>
+#include "zipper/concepts/Matrix.hpp"
 
 namespace balsa::geometry::simplex {
 
 
-template<::zipper::concepts::MatrixBaseDerived MatType>
+template<::zipper::concepts::Matrix MatType>
     requires(MatType::extents_traits::is_dynamic || MatType::extents_type::static_extent(0) + 1 == MatType::extents_type::static_extent(1))
 auto circumcenter_spd(const MatType &V);
-template<::zipper::concepts::MatrixBaseDerived MatType>
+template<::zipper::concepts::Matrix MatType>
 auto circumcenter_spsd(const MatType &V);
 
 
-template<::zipper::concepts::MatrixBaseDerived MatType>
+template<::zipper::concepts::Matrix MatType>
 auto circumcenter(const MatType &V);
 
-template<::zipper::concepts::MatrixBaseDerived SimplexVertices>
+template<::zipper::concepts::Matrix SimplexVertices>
 auto circumcenter_with_squared_radius(const SimplexVertices &S);
 
-template<::zipper::concepts::MatrixBaseDerived SimplexVertices>
+template<::zipper::concepts::Matrix SimplexVertices>
 auto circumcenter_with_radius(const SimplexVertices &S);
 
 
 namespace detail {
 
+    // Back-substitution for upper triangular system R*x = b.
+    // Near-zero pivots are treated as zero (for rank-deficient systems).
+    template<::zipper::concepts::Matrix RMat, ::zipper::concepts::Matrix BVec>
+    auto upper_triangular_solve(const RMat &R, const BVec &b) {
+        using value_type = typename RMat::value_type;
+        constexpr auto N = RMat::static_extent(1);
+        ::zipper::Vector<value_type, N> x(R.extent(1));
+        const value_type pivot_tol = std::numeric_limits<value_type>::epsilon() * 100;
+        for (::zipper::index_type i = R.extent(1) - 1; i >= 0; --i) {
+            value_type s = b(i);
+            for (::zipper::index_type j = i + 1; j < R.extent(1); ++j) {
+                s -= R(i, j) * x(j);
+            }
+            x(i) = (std::abs(R(i, i)) > pivot_tol) ? s / R(i, i) : value_type(0);
+        }
+        return x;
+    }
 
-    template<::zipper::concepts::MatrixBaseDerived MatType>
+    // Solve A*x = b via QR decomposition with back-substitution.
+    template<::zipper::concepts::Matrix AMat, ::zipper::concepts::Matrix BVec>
+    auto qr_solve(const AMat &A, const BVec &b) {
+        auto [Q, R] = ::zipper::utils::decomposition::qr(A);
+        auto Qtb = (Q.transpose() * b).eval();
+        return upper_triangular_solve(R, Qtb);
+    }
+
+    // Check if the QR R factor indicates a rank-deficient matrix
+    // (any diagonal element near zero).
+    template<::zipper::concepts::Matrix RMat>
+    bool qr_is_degenerate(const RMat &R) {
+        using value_type = typename RMat::value_type;
+        const value_type tol = std::numeric_limits<value_type>::epsilon() * 100;
+        ::zipper::index_type n = std::min(R.extent(0), R.extent(1));
+        for (::zipper::index_type i = 0; i < n; ++i) {
+            if (std::abs(R(i, i)) < tol) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    template<::zipper::concepts::Matrix MatType>
     auto circumcenter_spsd(const MatType &V) {
         using value_type = typename MatType::value_type;
         constexpr static ::zipper::index_type compile_cols = MatType::static_extent(1);
@@ -49,7 +86,7 @@ namespace detail {
         auto A = MType(simplex_count + 1, simplex_count + 1);
 
 
-        auto ones = ::zipper::views::nullary::ConstantView<value_type>(1);
+        auto ones = ::zipper::expression::nullary::Constant<value_type>(1);
         A.row(simplex_count).head(A.extent(0) - 1) = ones;
         A.col(simplex_count).head(A.extent(1) - 1) = ones;
         A(simplex_count, simplex_count) = 0;
@@ -69,19 +106,14 @@ namespace detail {
         }
         auto b = V.colwise().norm_powered().homogeneous().eval();
 
-        auto Ae = eigen::as_eigen(A);
-
-        auto be = eigen::as_eigen(b).eval();
-        auto xe = Ae.colPivHouseholderQr().solve(be).eval();
-
-        auto x = eigen::as_zipper(xe);
+        auto x = qr_solve(A, b);
 
         return (V * x.head(V.cols())).eval();
     }
 
     // should be a N,N+1 shape matrix
     // if RowCol static then get a shape guarantee
-    template<::zipper::concepts::MatrixBaseDerived MatType>
+    template<::zipper::concepts::Matrix MatType>
         requires(MatType::extents_traits::is_dynamic || (MatType::extents_type::static_extent(0) + 1 == MatType::extents_type::static_extent(1)))
     auto circumcenter_spd(const MatType &V) {
         // 2 V.dot(C) = sum(V.colwise().squaredNorm()).transpose()
@@ -117,39 +149,34 @@ namespace detail {
         auto b = m.colwise().norm_powered().eval();
         auto A = (2 * m.transpose() * m).eval();
 
-        auto Ae = eigen::as_eigen(A);
-
-        auto llt = Ae.llt();
-        if (llt.info() != Eigen::ComputationInfo::Success) {
+        auto [Q, R] = ::zipper::utils::decomposition::qr(A);
+        if (qr_is_degenerate(R)) {
             spdlog::debug("circumcenter_spd: Degenerate simplex detected, using circumcenter_spsd");
             return circumcenter_spsd(V);
         }
-        auto be = eigen::as_eigen(b).eval();
 
-        llt.solveInPlace(be);
+        auto Qtb = (Q.transpose() * b).eval();
+        auto sol = upper_triangular_solve(R, Qtb);
 
-        b = eigen::as_zipper(be);
-
-        auto c = (V.col(0) + m * b).eval();
-        return c;
+        return (V.col(0) + m * sol).eval();
     }
 
 }// namespace detail
 
 
-template<::zipper::concepts::MatrixBaseDerived MatType>
+template<::zipper::concepts::Matrix MatType>
     requires(MatType::extents_traits::is_dynamic || MatType::extents_type::static_extent(0) + 1 == MatType::extents_type::static_extent(1))
 auto circumcenter_spd(const MatType &V) {
 
     return detail::circumcenter_spd(V);
 }
-template<::zipper::concepts::MatrixBaseDerived MatType>
+template<::zipper::concepts::Matrix MatType>
 auto circumcenter_spsd(const MatType &V) {
     return detail::circumcenter_spsd(V);
 }
 
 
-template<::zipper::concepts::MatrixBaseDerived MatType>
+template<::zipper::concepts::Matrix MatType>
 auto circumcenter(const MatType &V) {
     // auto v = eigen::as_eigen(V);
     // auto c = circumcenter(v);
@@ -179,13 +206,13 @@ auto circumcenter(const MatType &V) {
     }
 }
 
-template<::zipper::concepts::MatrixBaseDerived SimplexVertices>
+template<::zipper::concepts::Matrix SimplexVertices>
 auto circumcenter_with_squared_radius(const SimplexVertices &S) {
     auto C = circumcenter(S);
     return std::make_tuple(C, (C - S.col(0)).template norm_powered<2>());
 }
 
-template<::zipper::concepts::MatrixBaseDerived SimplexVertices>
+template<::zipper::concepts::Matrix SimplexVertices>
 auto circumcenter_with_radius(const SimplexVertices &S) {
     auto C = circumcenter(S);
     return std::make_tuple(C, (C - S.col(0)).norm());
