c5ef9038f1bcb210200d41381ce7f2a9e8fc484c
Michael Tao <michael.w.tao@gmail.com>
2026-03-15 13:36:39 -0400

    fix circumcenters batch return type (Vector -> Matrix) and clean up volumes

--- geometry/include/balsa/geometry/simplicial_complex/circumcenters.hpp
+++ geometry/include/balsa/geometry/simplicial_complex/circumcenters.hpp
@@ -3,6 +3,14 @@
 #include "balsa/geometry/simplex/circumcenter.hpp"
 
 namespace balsa::geometry::simplicial_complex {
+
+/// Compute the circumcenter of each simplex in S, given vertex positions V.
+///
+/// @param V  Position matrix (D rows x num_vertices columns).
+/// @param S  Simplex index matrix (K+1 rows x num_simplices columns),
+///           integral-valued.
+/// @return   Matrix (D rows x num_simplices columns) where each column
+///           is the circumcenter of the corresponding simplex.
 template<
   ::zipper::concepts::Matrix PosType,
   ::zipper::concepts::Matrix SimplexType>
@@ -11,17 +19,14 @@ auto circumcenters(const PosType &V, const SimplexType &S) {
 
     using value_type = typename PosType::value_type;
 
-    auto cols = S.cols();
+    constexpr static zipper::index_type compile_rows = PosType::extents_type::static_extent(0);
     constexpr static zipper::index_type compile_cols = SimplexType::extents_type::static_extent(1);
-    balsa::zipper::Vector<value_type, compile_cols> C(cols);
+    balsa::zipper::Matrix<value_type, compile_rows, compile_cols> C(V.extent(0), S.extent(1));
 
-    // tbb::parallel_for(int(0), int(S.cols()), [&](int j) {
-    //     auto s = S.col(j);
-    //     C(j) = volume(V(Eigen::all, s));
-    // });
     for (zipper::index_type j = 0; j < S.extent(1); ++j) {
         auto s = S.col(j);
-        C(j) = simplex::circumcenter(V(::zipper::full_extent_t{}, s));
+        auto c = C.col(j);
+        c = simplex::circumcenter(V(::zipper::full_extent_t{}, s));
     }
     return C;
 }
--- geometry/include/balsa/geometry/simplicial_complex/volumes.hpp
+++ geometry/include/balsa/geometry/simplicial_complex/volumes.hpp
@@ -3,33 +3,26 @@
 #include "balsa/geometry/simplex/volume.hpp"
 
 namespace balsa::geometry::simplicial_complex {
+
+/// Compute the volume of each simplex in S, given vertex positions V.
+///
+/// @param V  Position matrix (D rows x num_vertices columns).
+/// @param S  Simplex index matrix (K+1 rows x num_simplices columns),
+///           integral-valued.
+/// @return   Vector of length num_simplices containing per-simplex volumes.
+///           Signed when D+1 == K+1 (i.e. full-rank simplices), unsigned
+///           otherwise.
 template<
   ::zipper::concepts::Matrix PosType,
   ::zipper::concepts::Matrix SimplexType>
     requires(std::is_integral_v<typename SimplexType::value_type>)
 auto volumes(const PosType &V, const SimplexType &S) {
 
-    /*
-    constexpr static int cols = eigen::concepts::detail::compile_col_size<SimplexType>;
-    using RetType = eigen::RowVector<typename PosType::Scalar, cols>;
-
-    RetType R(S.cols());
-
-    for (int j = 0; j < R.size(); ++j) {
-        R(j) = simplex::volume(V(Eigen::all, S.col(j)));
-    }
-    return R;
-    */
     using value_type = typename PosType::value_type;
 
-    auto cols = S.cols();
     constexpr static zipper::index_type compile_cols = SimplexType::extents_type::static_extent(1);
-    balsa::zipper::Vector<value_type, compile_cols> C(cols);
+    balsa::zipper::Vector<value_type, compile_cols> C(S.extent(1));
 
-    // tbb::parallel_for(int(0), int(S.cols()), [&](int j) {
-    //     auto s = S.col(j);
-    //     C(j) = volume(V(Eigen::all, s));
-    // });
     for (zipper::index_type j = 0; j < S.extent(1); ++j) {
         auto s = S.col(j);
         C(j) = simplex::volume(V(::zipper::full_extent_t{}, s));
